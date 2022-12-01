623a46278c5d814f049a0b4465c6ddb9eca03840
ccrismota <ccrismota@gmail.com>
2022-12-01 17:09:33 -0300

    Criação dos componentes

---
+++ src/app/components/curriculo/curriculo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curriculo',
+  templateUrl: './curriculo.component.html',
+  styleUrls: ['./curriculo.component.sass']
+})
+export class CurriculoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
