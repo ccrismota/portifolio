ad79890fd6eb910a0373233a97cbd077aff75238
ccrismota <ccrismota@gmail.com>
2022-12-14 23:22:18 -0300

    alteracoes e implementacoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { CurriculoComponent } from './components/curriculo/curriculo.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PortifolioComponent } from './components/portifolio/portifolio.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
-const routes: Routes = [];
+
+const appRoutes: Routes = [
+  { path: '', component: HeaderComponent,
+
+  children: [
+    { path: 'sobre', component: SobreComponent },
+    { path: 'portifolio', component: PortifolioComponent },
+    { path: 'contato', component: ContatoComponent },
+    { path: 'curriculo', component: CurriculoComponent },
+  ]},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+    appRoutes, { enableTracing: true }
+    )
+    ],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { BodyComponent } from './components/body/body.component';
 import { SobreComponent } from './components/sobre/sobre.component';
 import { PortifolioComponent } from './components/portifolio/portifolio.component';
 import { ContatoComponent } from './components/contato/contato.component';
 import { CurriculoComponent } from './components/curriculo/curriculo.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent,
     SobreComponent,
     PortifolioComponent,
     ContatoComponent,
-    CurriculoComponent
+    CurriculoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
