57400259db0dc79e07392c17cfd570db26fba407
ccrismota <ccrismota@gmail.com>
2024-05-17 18:31:46 -0300

    fix: refatoração do código da app-content

--- src/app/components/app-content/app-content.component.ts
+++ src/app/components/app-content/app-content.component.ts
@@ -11,57 +11,75 @@ import { ServiceService } from 'src/app/services/service.service';
 export class AppContentComponent implements OnInit, OnDestroy {
 
   showX: boolean = false;
-  heightHeader = '100vh';
-  topContainerFluid = '167px';
+  heightHeader: string = '100vh';
+  topContainerFluid: string = '167px';
   showBotao: boolean = false;
   menuContato: string = 'none';
 
   @Output() marginTopBodyNavbar = '165px';
 
-  private observeSubscription: Subscription;
-
+  private observeMenuSubscription: Subscription;
+  private observeHeightSubscription: Subscription;
   constructor(
     private service: ServiceService
   ) {
-    this.observeSubscription = this.service.observeMenu$.subscribe((os) => { this.menuContato = os; });
-    this.observeSubscription = this.service.observeHeight$.subscribe((os) => { this.heightHeader = os; });
+    this.observeMenuSubscription = this.service.observeMenu$.subscribe((os) => { this.menuContato = os; });
+    this.observeHeightSubscription = this.service.observeHeight$.subscribe((os) => { this.heightHeader = os; });
   }
 
   ngOnDestroy(): void {
-    this.observeSubscription.unsubscribe();
+    this.observeMenuSubscription.unsubscribe();
+    this.observeHeightSubscription.unsubscribe();
   }
 
-
   @HostListener('window:resize', ['$event'])
-  updateSize(event: any) {
+  updateSize(event: any): void {
+    this.toggleShowX();
+    this.updateLayout();
+  }
+
+  private toggleShowX(): void {
     this.showX = !this.showX;
     this.showBotao = !this.showBotao;
+  }
+
+  private updateLayout(): void {
     if (window.innerWidth < 768) {
-      this.menuContato = 'none';
-      this.heightHeader = '362px';
-      this.topContainerFluid = '205px';
-      if (window.innerWidth < 768 && this.showX === true) {
-        this.menuContato = 'block';
-      } else {
-        this.menuContato = 'none';
-        this.heightHeader = '162px';
-      }
+      this.updateMobileLayout();
     } else {
-      this.heightHeader = '100vh';
+      this.updateDesktopLayout();
+    }
+  }
+
+  private updateMobileLayout(): void {
+    this.menuContato = 'none';
+    this.heightHeader = '362px';
+    this.topContainerFluid = '205px';
+    if (this.showX) {
       this.menuContato = 'block';
-      this.showX = true;
-    } if (this.showX) {
-      this.showBotao = true;
       this.topContainerFluid = '405px';
       this.marginTopBodyNavbar = '365px';
     } else {
-      this.topContainerFluid = '205px';
+      this.menuContato = 'none';
+      this.heightHeader = '162px';
       this.marginTopBodyNavbar = '165px';
     }
-
   }
 
-  ngOnInit(): void {
+  private updateDesktopLayout(): void {
+    this.heightHeader = '100vh';
+    this.menuContato = 'block';
+    this.showX = true;
+    this.showBotao = true;
+    // this.topContainerFluid = '405px';
+    // this.marginTopBodyNavbar = '365px';
+  }
 
+
+  ngOnInit(): void {
+    if (window.innerWidth < 768) {
+      this.menuContato = 'none';
+      this.topContainerFluid = '205px';
+    }
   }
 }
